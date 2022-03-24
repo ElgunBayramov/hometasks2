034d012a5ea341fadf116b6bf18cc3e37e99ec85
Elguncode <elgunmb@code.edu.az>
2022-03-24 23:22:24 +0400

    tapsiriq

---
+++ tasks-20220319/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3 reqemli eded verilib.
+            // Bu ededin axirina hemin ededdin ozunu yapishdir. example: 333= 333333;
+            Console.Write("eded daxil edin: ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            if(a>99 && a <= 999)
+            {
+                Console.WriteLine($"{a}{a}");
+            }
+            else
+            {
+                Console.WriteLine("eded 3 reqemli deyil");
+            }
+        }
+    }
+}
