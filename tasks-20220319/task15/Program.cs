67d55bc048fafa1153f627227c3387104e0fd57d
Elguncode <elgunmb@code.edu.az>
2022-03-25 16:27:03 +0400

    tapsiriq

---
+++ tasks-20220319/task15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 7 dene eded verilib.Bunlardan 2 denesi 3 reqemlidir. 2 denesi 4 reqemlidir. 2 denesi 5 reqemlidir.1 denesi ise 6 reqemlidir.
+            3 reqemli ededleri topla , sonra bunlarin ustune gel 4 reqemli ededlerin bir birine vurulmasindan alinan cavabi.
+            Sonra neticenin axirina 7 reqemini artir. Sonra cavabin ustune gel 5 reqemli ededlerin cemini.
+            Sonra neticeden cix 3 reqemli ededlerin bir birine vurulmasindan alinan cavabin axirina 1 artirilmish variantini.
+            Sonra alinan cavabin ustune gel 6 reqemli ededi.
+            Sonra alinan cavabdan cix 3 reqemli ve 4 reqemli ededlerin cemini.
+            Sonra alinan cavabin 18 % -ni sonra 3 % -ni sonra 1 % tap.
+            Sonra alinan cavabin ustune 5 reqemli ededlerin cemini gel
+            */
+
+        }
+    }
+}
